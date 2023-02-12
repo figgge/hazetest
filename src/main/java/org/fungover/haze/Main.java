5ade71c1b7c9a3ae499bc8ca99c07d629058cb49
Martin Blomberg <martin.blomberg@outlook.com>
2023-02-12 21:59:24 +0100

    Add HazeIT.java
    Integration tests that starts the server on a random free port.
    Tests for the following commands:
    PING - PONG
    SETNX
    SET - GET
    DELETE
    EXISTS
    
    Error for not implemented command

--- src/main/java/org/fungover/haze/Main.java
+++ src/main/java/org/fungover/haze/Main.java
@@ -10,77 +10,82 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Main {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        int port = 6379;
 
-		HazeDatabase hazeDatabase = new HazeDatabase();
+        if (args.length > 1 && args[0].equals("--port")) {
+            port = Integer.parseInt(args[1]);
+        }
+        System.out.println(port);
 
-		try (ServerSocket serverSocket = new ServerSocket(6379)) {
-			while (true) {
-				var client = serverSocket.accept();
+        HazeDatabase hazeDatabase = new HazeDatabase();
 
-				Runnable newThread = () -> {
-					try {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            while (true) {
+                var client = serverSocket.accept();
 
+                Runnable newThread = () -> {
+                    try {
+                        while (true) {
+                            BufferedReader input = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                            List<String> inputList = new ArrayList<>();
 
-                        BufferedReader input = new BufferedReader(new InputStreamReader(client.getInputStream()));
-                        List<String> inputList = new ArrayList<>();
+                            String firstReading = input.readLine();
+                            readInputStream(input, inputList, firstReading);
 
-                        String firstReading = input.readLine();
-                        readInputStream(input, inputList, firstReading);
+                            executeCommand(hazeDatabase, client, inputList);
 
-				        executeCommand(hazeDatabase, client, inputList);
+                            inputList.forEach(System.out::println); // For checking incoming message
 
-				        inputList.forEach(System.out::println); // For checking incoming message
+                            printThreadDebug();
+                        }
+                        //client.close();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                };
+                Thread.startVirtualThread(newThread);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-						printThreadDebug();
+    private static void printThreadDebug() {
+        System.out.println("ThreadID " + Thread.currentThread().threadId());  // Only for Debug
+        System.out.println("Is virtual Thread " + Thread.currentThread().isVirtual()); // Only for Debug
+    }
 
-						client.close();
+    private static void executeCommand(HazeDatabase hazeDatabase, Socket client, List<String> inputList) throws IOException {
+        String command = inputList.get(0);
+        String key = inputList.get(1);
+        String value = getValueIfExist(inputList);
 
-					} catch (IOException e) {
-						throw new RuntimeException(e);
-					}
-				};
-				Thread.startVirtualThread(newThread);
-			}
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-	}
+        if (command.equals("SETNX")) {
+            client.getOutputStream().write(hazeDatabase.setNX(key, value).getBytes());
+        } else {
+            client.getOutputStream().write("-ERR unknown command\r\n".getBytes());
+        }
+    }
 
-	private static void printThreadDebug() {
-		System.out.println("ThreadID " + Thread.currentThread().threadId());  // Only for Debug
-		System.out.println("Is virtual Thread " + Thread.currentThread().isVirtual()); // Only for Debug
-	}
+    private static String getValueIfExist(List<String> inputList) {
+        if (inputList.size() == 3)
+            return inputList.get(2);
+        return "";
+    }
 
-	private static void executeCommand(HazeDatabase hazeDatabase, Socket client, List<String> inputList) throws IOException {
-		String command = inputList.get(0);
-		String key = inputList.get(1);
-		String value = getValueIfExist(inputList);
-
-		switch (command) {
-			case "SETNX" -> client.getOutputStream().write(hazeDatabase.setNX(key, value).getBytes());
-			default -> client.getOutputStream().write("-ERR unknown command\r\n".getBytes());
-		}
-	}
-
-	private static String getValueIfExist(List<String> inputList) {
-		if (inputList.size() == 3)
-			return inputList.get(2);
-		return "";
-	}
-
-	private static void readInputStream(BufferedReader input, List<String> inputList, String firstReading) throws IOException {
-		int size;
-		if (firstReading.startsWith("*")) {
-			size = Integer.parseInt(firstReading.substring(1)) * 2;
-			for (int i = 0; i < size; i++) {
-				String temp = input.readLine();
-				if (!temp.contains("$"))
-					inputList.add(temp);
-			}
-		} else {
-			String[] seperated = firstReading.split("\\s");
-			inputList.addAll(Arrays.asList(seperated));
-		}
-	}
+    private static void readInputStream(BufferedReader input, List<String> inputList, String firstReading) throws IOException {
+        int size;
+        if (firstReading.startsWith("*")) {
+            size = Integer.parseInt(firstReading.substring(1)) * 2;
+            for (int i = 0; i < size; i++) {
+                String temp = input.readLine();
+                if (!temp.contains("$"))
+                    inputList.add(temp);
+            }
+        } else {
+            String[] seperated = firstReading.split("\\s");
+            inputList.addAll(Arrays.asList(seperated));
+        }
+    }
 }
